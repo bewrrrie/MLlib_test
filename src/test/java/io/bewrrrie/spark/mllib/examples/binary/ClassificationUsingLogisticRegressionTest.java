481a492bd7a5acb28046807597bfed39b4b5fb20
bewrrrie <silverr41@gmail.com>
2017-07-06 18:45:19 +0600

    Classification with logistic regression example - started!

---
+++ src/test/java/io/bewrrrie/spark/mllib/examples/binary/ClassificationUsingLogisticRegressionTest.java
@@ -0,0 +1,10 @@
+package io.bewrrrie.spark.mllib.examples.binary;
+
+import static org.junit.Assert.*;
+
+/**
+ *  Binary classification example test class
+ */
+public class ClassificationUsingLogisticRegressionTest {
+
+}
