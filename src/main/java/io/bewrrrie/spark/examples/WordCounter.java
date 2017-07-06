4e575855d6380e63e28d5aa127dc4c3689215900
bewrrrie <silverr41@gmail.com>
2017-07-06 17:39:10 +0600

    Word counter example - transformed into unreadable thing!

--- src/main/java/io/bewrrrie/spark/examples/WordCounter.java
+++ src/main/java/io/bewrrrie/spark/examples/WordCounter.java
@@ -3,8 +3,6 @@ package io.bewrrrie.spark.examples;
 import java.util.Arrays;
 import java.util.Map;
 import org.apache.spark.SparkContext;
-import org.apache.spark.api.java.JavaPairRDD;
-import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.function.FlatMapFunction;
 import org.apache.spark.api.java.function.Function2;
 import org.apache.spark.api.java.function.PairFunction;
@@ -15,10 +13,10 @@ import scala.Tuple2;
  */
 public final class WordCounter {
 	public static Map<String, Integer> countWord(final SparkContext sc, final String filePath) {
-		final JavaRDD<String> lines = sc.textFile(filePath, 1).toJavaRDD();
-		final JavaRDD<String> words = lines.flatMap((FlatMapFunction<String, String>) s -> Arrays.asList(s.split(" ")).iterator());
-		final JavaPairRDD<String, Integer> ones = words.mapToPair((PairFunction<String, String, Integer>) s -> new Tuple2<>(s, 1));
-		final JavaPairRDD<String, Integer> counts = ones.reduceByKey((Function2<Integer, Integer, Integer>) (x, y) -> x + y);
-		return counts.collectAsMap();
+		return sc.textFile(filePath, 1).toJavaRDD()
+            .flatMap((FlatMapFunction<String, String>) s -> Arrays.asList(s.split(" ")).iterator())
+            .mapToPair((PairFunction<String, String, Integer>) s -> new Tuple2<>(s, 1))
+            .reduceByKey((Function2<Integer, Integer, Integer>) (x, y) -> x + y)
+            .collectAsMap();
 	}
 }
